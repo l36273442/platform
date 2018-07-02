8d83a06dd51f611939ec655b01a5c458a9c4d3c2
l36273442 <l36273442@163.com>
2018-07-02 16:29:13 +0800

    add

--- platform/protected/modules/admin/controllers/LoginController.php
+++ platform/protected/modules/admin/controllers/LoginController.php
@@ -2,119 +2,43 @@
 class LoginController extends CommonController{
     
 	public function actionLogin(){
-        if( isset(Yii::app()->session['id']) && !empty( Yii::app()->session['id'] ) && Yii::app()->session['expire'] > time() ){
+        if( isset(Yii::app()->session['admin_id']) && !empty( Yii::app()->session['admin_id'] ) ){
             $this->redirect(Yii::app()->getBaseUrl().'/site/index');
         }    
         $this->render('login');
     }
     public function actionLoginOut(){
-        if( isset(Yii::app()->session['id'] )){
-            unset(Yii::app()->session['id']);
+        if( isset(Yii::app()->session['admin_id'] )){
+            unset(Yii::app()->session['admin_id']);
         }
-        if( isset(Yii::app()->session['phone'] )){
-            unset(Yii::app()->session['phone']);
+        if( isset(Yii::app()->session['admin_name'] )){
+            unset(Yii::app()->session['admin_name']);
         }
-        if( isset(Yii::app()->session['expire'] )){
-            unset(Yii::app()->session['expire']);
-        }
-        $this->renderJson(Yii::t('common','success'));
-
-    }
-    public function actionForgetPassword(){
-        if( isset(Yii::app()->session['id']) && !empty( Yii::app()->session['id'] ) && Yii::app()->session['expire'] > time() ){
-            $this->redirect(Yii::app()->getBaseUrl().'/site/index');
-        }    
-        $this->render('forgetpassword');
-    }
-    public function actionDoForgetPassword(){
-        $p = $this->getParams('POST');
-        if( !isset($p['country_code']) || empty($p['country_code']) ){
-            $this->renderError(Yii::t('common','country_code_empty'), ErrorCode::PARAM_EMPTY); 
-        }
-        if( !isset($p['img_code']) || empty($p['img_code']) ){
-            $this->renderError(Yii::t('common','img_code_empty'), ErrorCode::PARAM_EMPTY); 
-        }
-        if( $p['img_code'] != $_SESSION['code'] ){
-            $this->renderError(Yii::t('common','img_code_err'), ErrorCode::PARAM_ERROR); 
-        }
-        if( $_SESSION['code_timeout'] < time() ){
-            unset(Yii::app()->session['code']);
-            unset(Yii::app()->session['code_timeout']);
-            $this->renderError(Yii::t('common','img_code_timeout'), ErrorCode::PARAM_ERROR); 
-        }
-        if( !isset($p['sms_code']) || empty($p['sms_code']) ){
-            $this->renderError(Yii::t('common','sms_code_empty'), ErrorCode::PARAM_EMPTY); 
-        }
-        if( $p['sms_code'] != $_SESSION['sms_forget_code'] ){
-            $this->renderError(Yii::t('common','sms_code_err'), ErrorCode::PARAM_ERROR); 
-        }
-        if( $_SESSION['sms_forget_code_time'] + SMS_FORGET_EXPIRE*60 < time() ){
-            unset(Yii::app()->session['sms_forget_code']);
-            unset(Yii::app()->session['sms_forget_code_time']);
-            $this->renderError(Yii::t('common','sms_code_timeout'), ErrorCode::PARAM_ERROR); 
-        }
-        if( !isset($p['mobile']) || empty($p['mobile']) ){
-            $this->renderError(Yii::t('common','account_empty'), ErrorCode::PARAM_EMPTY); 
-        }
-        if( !isset($p['password']) || empty($p['password']) ){
-            $this->renderError(Yii::t('common','user_password_err'), ErrorCode::PARAM_EMPTY); 
-        }
-        if( strlen($p['password']) < 6 || strlen($p['password']) > 16 ){
-            $this->renderError(Yii::t('common','password_len'), ErrorCode::PARAM_ERROR); 
-        }
-        if( !$this->check_password($p['password']) ){
-            $this->renderError(Yii::t('common','password_type'), ErrorCode::PARAM_ERROR); 
-        }
-        $p['country_code'] = trim($p['country_code']);
-        $p['mobile'] = trim($p['mobile']);
-
-        $user = UserModel::model()->getUserByPhone( $p['country_code'] , $p['mobile']) ;
-        if( !$user ){
-            $this->renderError(Yii::t('common','account_not_exists'), ErrorCode::USERS_ERROR);
-        }
-        $pass = md5( $p['password'] ); 
-        if( $pass == $user['password'] ){
-            $this->renderError(Yii::t('common','password_same'), ErrorCode::PARAM_EMPTY);  
-        }
-        $re = UserModel::model()->updateByPk($user['id'] , array('password' => $pass , 'uptime' => time()));
-        if( $re ){
-            unset(Yii::app()->session['sms_forget_code']);
-            unset(Yii::app()->session['sms_forget_code_time']);
-            $this->renderJson(Yii::t('common','success'));
-        }
-        else{
-            $this->renderError(Yii::t('common','error') , ErrorCode::SYSTEM_ERROR);
-
-        }
-
+        $this->renderJson('成功');
 
     }
     public function actionDoLogin(){
-        $p = $this->getParams('POST');
-        if( !isset($p['country_code']) || empty($p['country_code']) ){
-            $this->renderError(Yii::t('common','country_code_empty'), ErrorCode::PARAM_EMPTY); 
-        }
-        if( !isset($p['mobile']) || empty($p['mobile']) ){
-            $this->renderError(Yii::t('common','account_empty'), ErrorCode::PARAM_EMPTY); 
+        $p = $this->getParams('GET');
+        if( !isset($p['name']) || empty($p['name']) ){
+            $this->renderError('用户名不能为空', ErrorCode::PARAM_EMPTY); 
         }
         if( !isset($p['password']) || empty($p['password']) ){
-            $this->renderError(Yii::t('common','user_password_err'), ErrorCode::PARAM_EMPTY); 
+            $this->renderError('密码不能为空', ErrorCode::PARAM_EMPTY); 
         }
-        $re = UserModel::model()->getUserByPhone( $p['country_code'] , $p['mobile']) ;
+        $re = AdminUserModel::model()->find('admin_name=:admin_name', array(':admin_name'=> $p['name']));
         if( empty($re) ){
-            $this->renderError(Yii::t('common','account_empty'), ErrorCode::USERS_ERROR);
+            $this->renderError( '无此用户', ErrorCode::USERS_ERROR);
         }
         if( $re['status'] != 0 ){
-            $this->renderError(Yii::t('common','user_deny'), ErrorCode::USERS_DENY);
+            $this->renderError('用户状态错误，请联系管理员', ErrorCode::USERS_DENY);
         }
         if( md5($p['password']) != $re['password']){
-            $this->renderError( Yii::t('common','user_password_err'), ErrorCode::USERS_PASSWORD_ERR );
+            $this->renderError( '密码错误', ErrorCode::USERS_PASSWORD_ERR );
         }
-        Yii::app()->session['phone'] = $re['phone'];
-        Yii::app()->session['id'] = $re['id'];
-        Yii::app()->session['expire'] = time() + Yii::app()->session->timeout;;
-        $this->user = $re;
+        Yii::app()->session['admin_name'] = $re['admin_name'];
+        Yii::app()->session['admin_id'] = $re['id'];
+        $this->admin_user = $re;
 
-        $this->renderJson(Yii::t('common','success'));
+        $this->renderJson('登录成功');
     }
 }
