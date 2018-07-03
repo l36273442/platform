2165d8124a339bffe402694563f88e2a89fbae35
l36273442 <l36273442@163.com>
2018-07-03 10:45:26 +0800

    add view

--- platform/protected/views/site/index.php
+++ platform/protected/views/site/index.php
@@ -1,11 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+    <link rel="stylesheet" href="./css/layui.css">
+    <link rel="stylesheet" href="./css/reset.css">
+    <script src="./js/jquery-3.3.1.min.js"></script>
+    <script src="./js/layui.js"></script>
+    <style>
+        .test{
+            width: 200px;
+            height: 200px;
+            background: red;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
 
+<div class="test"></div>
 
-<br /><br />
-<h1>用户</h1>
-<span><a href="/login/login" target="_blank" >登陆</a> </span> <span><a href="/register/register" target="_blank" >注册</a> </span> 
-<span><a href="/login/loginout" target="_blank" >退出</a> </span> <span><a href="/userview/powercontractorderlist" target="_blank" >用户合约订单</a> </span>
-<span><a href="/userview/uptpass" target="_blank" >修改交易密码</a></span> <span><a href="/userview/uppass" target="_blank" >修改登录密码</a></span>
-<span> <a href="/login/forgetpassword" target="_blank" >忘记密码</a></span>
-
-<h1>算力合约</h1>
-<span><a href="/powercontractview/list" target="_blank" >算力合约</a> </span>
+</body>
+</html>
