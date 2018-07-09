171036e3f7de418c9f74c3153db65649b381815c
linmin19951020 <1090048820@qq.com>
2018-07-09 16:39:51 +0800

    '左侧导航修改'

--- platform/protected/modules/admin/views/nav_left.php
+++ platform/protected/modules/admin/views/nav_left.php
@@ -17,12 +17,9 @@
             <!-- 左侧导航区域（可配合layui已有的垂直导航） -->
             <ul class="layui-nav layui-nav-tree" lay-filter="test">
                 <li class="layui-nav-item layui-nav-itemed">
-                    <a class="" href="javascript:;">所有商品</a>
+                    <a class="" href="javascript:;">用户</a>
                     <dl class="layui-nav-child">
-                        <dd><a href="javascript:;">列表一</a></dd>
-                        <dd><a href="javascript:;" class="list2">列表二</a></dd>
-                        <dd><a href="javascript:;">列表三</a></dd>
-                        <dd><a href="">超链接</a></dd>
+                        <dd><a href="index.php">用户列表</a></dd>
                     </dl>
                 </li>
                 <li class="layui-nav-item">
