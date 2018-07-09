a6c4245caea7882a80313ecc9795b4a29e1327f2
linmin19951020 <1090048820@qq.com>
2018-07-09 21:26:57 +0800

    'index修改'

--- platform/protected/modules/admin/views/site/index.php
+++ platform/protected/modules/admin/views/site/index.php
@@ -249,12 +249,9 @@
 
 <script src="/js/jquery-3.3.1.min.js"></script>
 <script src="/dist/layui.all.js"></script>
-<script type="text/html" id="barDemo">
-    <a class="layui-btn layui-btn-primary layui-btn-xs" lay-event="detail">查看</a>
-    <a class="layui-btn layui-btn-xs" lay-event="edit">编辑</a>
-    <a class="layui-btn layui-btn-danger layui-btn-xs" lay-event="del">删除</a>
-</script>
 <script>
+
+var element = layui.element;
 
     // 搜索
     $('.layui-icon-search').on('click',function () {
