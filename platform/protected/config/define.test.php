129d44fcefdd9cccf6c4296e991e1b554b4663fc
l36273442 <l36273442@163.com>
2018-07-10 14:11:46 +0800

    add

--- platform/protected/config/define.test.php
+++ platform/protected/config/define.test.php
@@ -5,8 +5,8 @@ define('SYSTEM_LOG_PATH', dirname(__FILE__).'/../runtime/logs/');
 define('UPLOAD_DIRECTORY',dirname(__FILE__).'/../../uploads/');
 define('TMP',dirname(__FILE__).'/../../tmp/');
 define('HEYSKY_API','http://api2.santo.cc');
-define('HEYSKY_CPID','f79m7g');
-define('HEYSKY_CPPWD','OrsAjAVm');
+define('HEYSKY_CPID','mz9p7n');
+define('HEYSKY_CPPWD','X2v0Xovb');
 define('SMS_EXPIRE', 10);//10分钟修改交易密码短信验证码有效期
 define('SMS_SEND_INTERVAL', 60);//短信时间发送间隔
 
