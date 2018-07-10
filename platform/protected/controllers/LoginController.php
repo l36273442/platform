1d12b330906d93fb79e8942f722c5e4fc7de6fa8
l36273442 <l36273442@163.com>
2018-07-10 14:54:12 +0800

    add

--- platform/protected/controllers/LoginController.php
+++ platform/protected/controllers/LoginController.php
@@ -118,13 +118,12 @@ class LoginController extends CommonController{
         if( empty($re) ){
             $this->renderError(Yii::t('common','account_empty'), ErrorCode::USERS_ERROR);
         }
-        unset(Yii::app()->session['code']);
-        unset(Yii::app()->session['code_timeout']);
         unset(Yii::app()->session['sms_code']);
         unset(Yii::app()->session['sms_code_time']);
         if( $re['status'] != 0 ){
             $this->renderError(Yii::t('common','user_deny'), ErrorCode::USERS_DENY);
         }
+        setcookie("id", $re['id'], time()+86400);
         Yii::app()->session['phone'] = $re['phone'];
         Yii::app()->session['id'] = $re['id'];
         $this->user = $re;
