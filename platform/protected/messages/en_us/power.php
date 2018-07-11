bc76cb8d6c0fd9cd0765d293727c3bd0988afb3f
l36273442 <l36273442@163.com>
2018-07-11 14:44:28 +0800

    add

---
+++ platform/protected/messages/en_us/power.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'all_network_power'     => 'Whole Network Hash',
+    'next_difficulty_time'  => 'Next Difficulty Remain Time',
+    'next_last_block'       => 'Next Difficulty Remain Block',
+    'seven_median'          => 'Block Median in Seven Day',
+);
---
+++ platform/protected/messages/zh_cn/power.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'all_network_power'     => '全网算力',
+    'next_difficulty_time'  => '下一难度剩余时间',
+    'next_last_block'       => '下一难度剩余块',
+    'seven_median'          => '近七日区块体积中位数',
+);
