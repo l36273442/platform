4680b483acc06fd698e0fbfc25d8277e153ffdad
l36273442 <l36273442@163.com>
2018-07-12 15:20:20 +0800

    add

--- platform/protected/controllers/MachinecontractviewController.php
+++ platform/protected/controllers/MachinecontractviewController.php
@@ -3,22 +3,26 @@ class MachineContractViewController extends CommonController{
 
     public $info;
     public function actionList(){
-        $this->render('list');
+        $this->render('list' , $this->data);
     }
     public function actionDetail(){
         $p = $this->getParams('REQUEST');
         if( !isset($p['id']) || !is_numeric($p['id']) || $p['id'] <= 0){
-            $this->renderError(Yii::t('common','param_error'), ErrorCode::PARAM_EMPTY); 
+            header('Location: '.Yii::app()->request->hostInfo);
+            exit;
         }
         
         $detail = MachineContractModel::model()->find( 'id=:id' , array(':id'=>$p['id']) );
-        $coin_info = coinModel::model()->find( 'id=:id' , array(':id'=>$detail['coin_id']) );
-        $unit_info = UnitModel::model()->find( 'id=:id' , array(':id'=>$detail['unit_id']) );
-        $machine_info = MiningMachineModel::model()->find( 'id=:id' , array(':id'=>$detail['machine_id']) );
-        $this->render('detail',array('info' => $detail->attributes,
-                                     'coin_info' => $coin_info->attributes,
-                                     'unit_info' => $unit_info->attributes,
-                                     'machine_info' => $machine_info,
-        )); 
+        $coin = coinModel::model()->find( 'id=:id' , array(':id'=>$detail['coin_id']) );
+        $unit = UnitModel::model()->find( 'id=:id' , array(':id'=>$coin['unit_id']) );
+        $machine = MiningMachineModel::model()->find( 'id=:id' , array(':id'=>$detail['machine_id']) );
+        $this->data['detail'] = empty($detail)?array():$detail->attributes;
+        $this->data['coin'] = empty($coin)?array():$coin->attributes;
+        $this->data['unit'] = empty($unit)?array():$unit->attributes;
+        $this->data['machine'] = empty($machine)?array():$machine->attributes;
+        
+        $this->render('detail',$this->data);
+          
+    
     }
 }
