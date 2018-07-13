88214c7b3f488a925c2d7ab2394f038afa93aea9
l36273442 <l36273442@163.com>
2018-07-13 15:24:51 +0800

    order

--- platform/protected/controllers/MachinecontractorderController.php
+++ platform/protected/controllers/MachinecontractorderController.php
@@ -46,4 +46,82 @@ class MachineContractOrderController extends AjaxController{
         }
         $this->renderJson(Yii::t('common','success') , $data);
     }
+    public function actionOrder(){
+        $p = $this->getParams('POST');
+        if( !isset($p['id']) || !is_numeric($p['id']) || $p['id']<=0 ){
+            $this->renderError(Yii::t('common','param_error') , ErrorCode::PARAM_ERROR);
+        }
+        if( !isset($p['count']) || !preg_match('/^[0-9]+$/', $p['count']) || $p['count']<=0 ){
+            $this->renderError(Yii::t('common','param_error') , ErrorCode::PARAM_ERROR);
+        }
+        $count = $p['count'];
+        $transaction = Yii::app()->db->beginTransaction();
+        try{
+            $mc = MachineContractModel::model()->findBySql( 'select * from '.MachineContractModel::model()->tableName().' where id=:id for update '  , array( ':id' => $p['id'] ) );
+            $user_coin = UserLegalCoinModel::model()->findBySql('select * from '.UserLegalCoinModel::model()->tableName().' where uid=:uid for update ' , array(':uid' => Yii::app()->session['id'] ));
+            if( empty($mc) ){
+                $transaction->rollback();
+                $this->renderError(Yii::t('common','system_error2'), ErrorCode::SYSTEM_ERROR); 
+            }
+            if( $mc->status != 0 ){
+                $transaction->rollback();
+                $this->renderError(Yii::t('common','contract_status_error'), ErrorCode::SYSTEM_ERROR); 
+            }
+            if( $mc->total <= $mc->deal_total + $count ){
+                $transaction->rollback();
+                $this->renderError(Yii::t('common','contract_not_enough'), ErrorCode::SYSTEM_ERROR); 
+            }
+            $m = MiningMachineModel::model()->find('id=:id', array(':id'=>$mc['machine_id']));
+            if(empty($m)){
+                $transaction->rollback();
+                $this->renderError(Yii::t('common','system_error2'), ErrorCode::SYSTEM_ERROR); 
+
+            }
+            $t = time();
+            $total = round($mc->price*$p['count'],8);
+            $order = new MachineContractOrderModel();
+            $order->mo_id = $mc->id;
+            $order->uid = Yii::app()->session['id'];
+            $order->machine_id = $mc->machine_id;
+            $order->coin_id = $mc->coin_id;
+            $order->price = $mc->price;
+            $order->order_price = $total;
+            $order->power = $m->power;
+            $order->total_power = $p['count']* $m->power;
+            $order->count = $p['count'];
+            $order->ctime = $t;
+            $order->uptime = $t;
+            $re = $order->save();            
+            $re2 = MachineContractModel::model()->updateCounters(array('deal_total'=>$count),'id=:id',array(':id'=>$p['id']));
+            $re3 = UserLegalCoinModel::model()->updateCounters(array('usd'=>-$total) , 'uid=:uid',array('uid'=>Yii::app()->session['id']));
+            $re4 = UserCoinModel::model()->updateCounters( array('total_machine'=>$count ,'machine_total_investment' => $total ,'total_investment' => $total) , 'uid=:uid and coin_id=:coin_id' , array('uid'=>Yii::app()->session['id'], ':coin_id' => $mc->coin_id ));
+
+            if( !$re || !$re2 || !$re3 || !$re4 ){
+                $transaction->rollback();
+                $this->renderError(Yii::t('common','order_fail'), ErrorCode::PARAM_EMPTY); 
+            }
+            $olog = new UserLegalCoinLogModel();
+            $olog->name = 'machine_buy';
+            $olog->coin_id = $mc->coin_id;
+            $olog->o_id = $order->id;
+            $olog->uid = Yii::app()->session['id'];
+            $olog->type = 1;
+            $olog->mining_type = 1;
+            $olog->vol = $total;
+            $olog->ctime= $t;
+            $re5 = $olog->save();
+            if( !$re5 ){
+                $transaction->rollback();
+                $this->renderError(Yii::t('common','order_fail2'), ErrorCode::PARAM_EMPTY); 
+            }
+            $transaction->commit(); 
+            $this->renderJson(Yii::t('common','success'));
+
+        }catch(Exception $e){
+            $transaction->rollback();
+            $this->renderError(Yii::t('common','system_error'), ErrorCode::SYSTEM_ERROR); 
+        }
+
+
+    }
 }
