f3b26c8a0f20506b956115f0d2b12a7948890be3
linmin19951020 <1090048820@qq.com>
2018-07-24 14:27:56 +0800

    'add'

--- platform/protected/views/login/signin.php
+++ platform/protected/views/login/signin.php
@@ -8,135 +8,149 @@
     <link rel="stylesheet" href="/css/intlTelInput.css">
     <link rel="stylesheet" href="/dist/css/layui.css">
     <style>
-        html,body{
-            width: 100%;
-            height: 100%;
-            background: #12101d;
-        }
-        .register{
-            width: 380px;
-            height: 360px;
-            background: #fff;
-            overflow: auto;
-            margin: auto;
-            position: absolute;
-            top: 0; left: 0; bottom: 0; right: 0;
-            border-radius: 5px;
-        }
-        .left{
-            width: 130px;
-            float: left;
-            height: 100%;
-            background: url("/images/左侧背景@2x.png")no-repeat;
-            background-size: 100% 100%;
-            position: relative;
-        }
-        .wrap{
-            height: 60px;
-            margin: auto;
-            position: absolute;
-            top: 0; left: 0; bottom: 0; right: 0;
-        }
-        .left P{
-            font-size: 14px;
-            font-weight: 600;
-            color: #d4d5da;
-            text-align: center;
-            line-height: 40px;
-        }
-        .left span{
-            float: left;
-            width: 100%;
-            color: #565564;
-            text-align: center;
-        }
-        .right{
-            float: right;
-            width: 226px;
-            position: relative;
-        }
-        .reg-title{
-            font-weight: 600;
-            font-size: 20px;
-            color:#05a0e8;
-            line-height: 86px;
-            margin-top: 8px;
-        }
-        .reg-center{
-            width: 180px;
-            height: 33px;
-            line-height: 33px;
-            border-left: 4px solid #009de9;
-            margin-bottom: 10px;
-            box-shadow: 5px 10px 10px #efeff0;
-            position: relative;
-        }
-        .intl-tel-input{
-            width: 70px;
-        }
-        .intl-tel-input .country-list{
-            width: 220px;
-        }
-        #country_code{
-            width: 100%;
-            height: 100%;
-        }
-        li.reg-center .tel{
-            width: 100px;
-            height: 32px;
-        }
-        .reg-center input{
-            border: none;
-            /* height: 100%;*/
-            width: 120px;
-            padding-left: 5px;
-        }
-        .security{
-            display: inline-block;
-            width: 40px;
-            height: 18px;
-            border: none;
-            line-height: 18px;
-            text-align: center;
-            position: absolute;
-            cursor: pointer;
-            font-family: HelveticaNeue;
-            top: 9px;
-            right: 8px;
-            font-size: 10px;
-            color: #fdfdfd;
-            background: #009dea;
-            border-radius: 6px;
-        }
-        .text{
-            line-height: 26px;
-            text-align: right;
-            margin-right: 43px;
-            font-family: HelveticaNeue;
-        }
-        .text a{
-            color: #009dea;
-        }
-        .up{
-            margin: 14px 0;
-        }
-        .login{
-            width: 180px;
-            height: 33px;
-            line-height: 33px;
-            color: #fff;
-            border: none;
-            background: #009dea;
-            border-radius: 3px;
-            cursor: pointer;
-        }
-        .already a{
-            position: absolute;
-            color: #67b9ef;
-            width: 180px;
-            text-align: center;
-            line-height: 20px;
-        }
+       html,body{
+           width: 100%;
+           height: 100%;
+           background: #12101d;
+       }
+       .register{
+           width: 380px;
+           height: 360px;
+           overflow: auto;
+           margin: auto;
+           position: absolute;
+           top: 0; left: 0; bottom: 0; right: 0;
+           border-radius: 5px;
+       }
+       .left{
+           width: 130px;
+           float: left;
+           height: 100%;
+           background: url("images/左侧背景@2x.png")no-repeat;
+           background-size: 100% 100%;
+           position: relative;
+       }
+       .wrap{
+           height: 60px;
+           margin: auto;
+           position: absolute;
+           top: 0; left: 0; bottom: 0; right: 0;
+       }
+       .left P{
+           font-size: 14px;
+           font-weight: 600;
+           color: #d4d5da;
+           text-align: center;
+           line-height: 40px;
+       }
+       .left span{
+           float: left;
+           width: 100%;
+           color: #565564;
+           text-align: center;
+       }
+       .right{
+           float: right;
+           width: 250px;
+           height: 100%;
+           background: #fff;
+       }
+       .reg-title{
+           font-weight: 600;
+           font-size: 20px;
+           color:#05a0e8;
+           line-height: 86px;
+           margin: 8px 0 0 20px;
+       }
+       .reg-center{
+           width: 180px;
+           height: 33px;
+           line-height: 33px;
+           border-left: 4px solid #009de9;
+           margin: 0 0 10px 20px;
+           box-shadow: 5px 10px 10px #efeff0;
+           position: relative;
+       }
+       .intl-tel-input{
+           width: 70px;
+       }
+       .intl-tel-input .country-list{
+           width: 220px;
+       }
+       #country_code{
+           width: 100%;
+           height: 100%;
+       }
+       li.reg-center .tel{
+           width: 100px;
+           height: 32px;
+       }
+       .reg-center input{
+          border: none;
+          color: #9c9c9c;
+          width: 120px;
+          padding-left: 5px;
+       }
+       .security{
+          display: inline-block;
+          width: 40px;
+          height: 18px;
+          border: none;
+          line-height: 18px;
+          text-align: center;
+          position: absolute;
+          cursor: pointer;
+          font-family: HelveticaNeue;
+          top: 9px;
+          right: 8px;
+          font-size: 10px;
+          color: #fdfdfd;
+          background: #009dea;
+          border-radius: 6px;
+       }
+       .text{
+          line-height: 26px;
+          text-align: right;
+          margin-right: 43px;
+          font-family: HelveticaNeue;
+       }
+       .text a{
+          color: #009dea;
+       }
+       .up{
+          margin: 14px 0 14px 20px;
+       }
+      .login{
+         width: 180px;
+         height: 33px;
+         line-height: 33px;
+         color: #fff;
+         border: none;
+         background: #009dea;
+         border-radius: 3px;
+         cursor: pointer;
+      }
+      .already a{
+         position: absolute;
+         color: #67b9ef;
+         width: 180px;
+         margin-left: 20px;
+         text-align: center;
+         line-height: 20px;
+      }
+      .close{
+         position: absolute;
+         right: 10px;
+         top: 8px;
+         width: 14px;
+         height: 14px;
+         cursor: pointer;
+      }
+      .close img{
+         width: 100%;
+         height: 100%;
+         background-size: 100% 100%;
+       }
     </style>
 </head>
 <body>
