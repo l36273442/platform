cd1b43c76cd0ceebd7a62ff279c05c219a1d0872
l36273442 <l36273442@163.com>
2018-07-24 19:16:46 +0800

    add

--- cron/gethbcoin.php
+++ cron/gethbcoin.php
@@ -5,10 +5,10 @@ echo "获取币种价格\n";
 echo "开始时间：".date("Y-m-d H:i:s")."\n";    
 require(dirname(__FILE__).'/common.php');
 $c = array('btc','eth');
-$url = huobi_pro_market.'/trade'; 
+$url = HOUBI_PRO_MARKET.'/trade'; 
 foreach($c as $v ){
     $p1 = $p2 = '';
-    $u .= '?symbol='.$v.'btc';
+    $u = $url.'?symbol='.$v.'btc';
     $d = @file_get_contents($u);
     if( !$d ){
         echo "获取币种交易信息失败".$u."\n";
