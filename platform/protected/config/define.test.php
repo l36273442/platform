d5e72adbeb54a348894496838369ef68cb9a756d
l36273442 <l36273442@163.com>
2018-07-24 19:21:06 +0800

    add

--- platform/protected/config/define.test.php
+++ platform/protected/config/define.test.php
@@ -9,6 +9,6 @@ define('HEYSKY_CPID','mz9p7n');
 define('HEYSKY_CPPWD','X2v0Xovb');
 define('SMS_EXPIRE', 10);//10分钟修改交易密码短信验证码有效期
 define('SMS_SEND_INTERVAL', 60);//短信时间发送间隔
-define('huobi_pro_market', 'https://api.huobi.pro/market');
-define('huobi_pro_api', 'https://api.huobi.pro/api');
+define('HOUBI_PRO_MARKET', 'https://api.huobi.pro/market');
+define('HUOBI_PRO_API', 'https://api.huobi.pro/api');
 
