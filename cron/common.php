ea33f57b9d769e512a19f27af8a29b6cf57b9fcd
l36273442 <l36273442@163.com>
2018-07-24 19:16:41 +0800

    add

--- cron/common.php
+++ cron/common.php
@@ -2,6 +2,7 @@
 set_time_limit(0);
 define( 'ENV', 'test' );
 $db_config = require(dirname(__FILE__).'/../platform/protected/config/db.'.ENV.'.php'); 
+require(dirname(__FILE__).'/../platform/protected/config/define.'.ENV.'.php'); 
 try {
 $db = new PDO( $db_config['connectionString'].';charset=UTF8', $db_config['username'], $db_config['password']);
 }catch (PDOException $e) {
